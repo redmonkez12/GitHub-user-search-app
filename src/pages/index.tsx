e9dae646975ce748155bce49cd1a3231a269abe1
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2023-04-21 00:57:24 +0200

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,177 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 
-const inter = Inter({ subsets: ['latin'] })
+import { Space_Mono } from "next/font/google";
+import Image from "next/image";
+import { getUserProfile, getUsers } from "@/services/user";
+
+const spaceMono = Space_Mono({ subsets: ["latin"], weight: ["400", "700"] });
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const { theme, setTheme } = useTheme();
+    const [themeInfo, setThemeInfo] = useState({
+        theme: "",
+    });
+    const [profile, setProfile] = useState<any>({});
+    const [searchText, setSearchText] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [noResults, setNoResults] = useState(false);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    useEffect(() => {
+        setThemeInfo({
+            theme: theme === "light" ? "dark" : "light",
+        });
+    }, [theme]);
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    async function onSubmit() {
+        setLoading(true);
+        const user = await getUsers(searchText);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        if (user) {
+            const login = user.login;
+            const profile = await getUserProfile(login);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+            const date = new Date(profile.created_at);
+            const options = { year: "numeric", month: "short", day: "numeric" };
+            // @ts-ignore
+            const formatter = new Intl.DateTimeFormat("en-US", options);
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            const parts = formatter.formatToParts(date);
+            const formattedDate = `Joined ${parts[2].value} ${parts[0].value} ${parts[4].value}`;
+
+            setProfile({
+                avatar: user.avatar_url,
+                username: login,
+                profileUrl: user.html_url,
+                repos: profile.public_repos,
+                followers: profile.followers,
+                following: profile.following,
+                name: profile.name,
+                createdAt: formattedDate,
+                bio: profile.bio,
+                company: profile.company,
+                twitter: profile.twitter_username,
+                blog: profile.blog,
+                location: profile.location,
+            });
+        } else {
+            setProfile({});
+            setNoResults(true);
+        }
+
+        setLoading(false);
+    }
+
+    function onChangeTheme() {
+        theme == "dark" ? setTheme("light") : setTheme("dark");
+    }
+
+    return (
+        <main
+            className={`${spaceMono.className} h-screen w-full px-6 py-8 flex md:w-[35.813rem] lg:w-[45.625rem] flex-col gap-4 mx-auto md:justify-center`}>
+            <div className={"md:w-[35.813rem] lg:w-[45.625rem]"}>
+                <header className={"flex justify-between mb-9"}>
+                    <h1 className={"text-2xl text-black dark:text-white font-bold"}>devfinder</h1>
+
+                    <div
+                        className={`flex items-center gap-4 cursor-pointer text-darkGray dark:text-white
+                            dark:hover:text-grayBlue dark:hover:fill-grayBlue dark:fill-white hover:text-black fill-lightGray hover:fill-black`}
+                        onClick={onChangeTheme}>
+                        <div
+                            className={"uppercase font-bold text-sm"}>{themeInfo.theme}</div>
+                        {themeInfo.theme === "dark" &&
+                            <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M19.513 11.397a.701.701 0 00-.588.128 7.496 7.496 0 01-2.276 1.336 7.101 7.101 0 01-2.583.462 7.505 7.505 0 01-5.32-2.209 7.568 7.568 0 01-2.199-5.342c0-.873.154-1.72.41-2.49a6.904 6.904 0 011.227-2.21.657.657 0 00-.102-.924.701.701 0 00-.589-.128C5.32.61 3.427 1.92 2.072 3.666A10.158 10.158 0 000 9.83c0 2.8 1.125 5.342 2.967 7.19a10.025 10.025 0 007.16 2.98c2.353 0 4.527-.822 6.266-2.183a10.13 10.13 0 003.58-5.624.623.623 0 00-.46-.796z"
+                                    fillRule="nonzero"/>
+                            </svg>
+                        }
+                        {themeInfo.theme === "light" &&
+                            <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
+                                <g fillRule="nonzero">
+                                    <path
+                                        d="M13.545 6.455c-.9-.9-2.17-1.481-3.545-1.481a4.934 4.934 0 00-3.545 1.481c-.9.9-1.481 2.17-1.481 3.545 0 1.376.582 2.646 1.481 3.545.9.9 2.17 1.481 3.545 1.481a4.934 4.934 0 003.545-1.481c.9-.9 1.481-2.17 1.481-3.545a4.934 4.934 0 00-1.481-3.545zM10 3.413a.7.7 0 00.688-.688V.688A.7.7 0 0010 0a.7.7 0 00-.688.688v2.037a.7.7 0 00.688.688zM15.635 5.344l1.455-1.455a.67.67 0 000-.952.67.67 0 00-.952 0l-1.455 1.455a.67.67 0 000 .952c.238.264.66.264.952 0zM19.312 9.312h-2.037a.7.7 0 00-.688.688.7.7 0 00.688.688h2.037A.7.7 0 0020 10a.7.7 0 00-.688-.688zM15.608 14.656a.67.67 0 00-.952 0 .67.67 0 000 .952l1.455 1.455a.67.67 0 00.952 0 .67.67 0 000-.952l-1.455-1.455zM10 16.587a.7.7 0 00-.688.688v2.037A.7.7 0 0010 20a.7.7 0 00.688-.688v-2.037a.7.7 0 00-.688-.688zM4.365 14.656L2.91 16.111a.67.67 0 000 .952.67.67 0 00.952 0l1.455-1.455a.67.67 0 000-.952c-.238-.264-.66-.264-.952 0zM3.413 10a.7.7 0 00-.688-.688H.688A.7.7 0 000 10a.7.7 0 00.688.688h2.037A.7.7 0 003.413 10zM4.365 5.344a.67.67 0 00.952 0 .67.67 0 000-.952L3.862 2.937a.67.67 0 00-.952 0 .67.67 0 000 .952l1.455 1.455z"/>
+                                </g>
+                            </svg>
+                        }
+                    </div>
+                </header>
+
+                <div>
+                    <div
+                        className={"input-wrapper rounded-2xl bg-white dark:bg-darkBlue flex items-center pr-2 pl-4"}>
+                        <Image src={"/images/icon-search.svg"} width={20} height={20} alt={"Search"}/>
+                        <input
+                            className={"py-4 px-2 md:px-4 md:py-6 text-sm flex-1 outline-none bg-white dark:bg-darkBlue"}
+                            placeholder={"Search GitHub username…"} type="text"
+                            onChange={(e) => {
+                                setNoResults(false);
+                                setSearchText(e.target.value);
+                            }}/>
+                        <div className={`${noResults ? "" : "invisible"} text-red pr-6`}>No results</div>
+                        <button onClick={onSubmit}
+                                disabled={loading}
+                                className={"text-white rounded-xl text-sm bg-blue font-bold px-4 md:py-4 md:px-7 py-3 hover:bg-lightBlue"}>Search
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <article
+                className={`md:w-[35.813rem] lg:w-[45.625rem] px-6 md:px-10 lg:px-12
+                pt-8 md:pt-10 md:pb-10 pb-12 bg-white dark:bg-darkBlue rounded-2xl w-full flex flex-col items-end ${profile.avatar ? "" : "invisible"}`}>
+                <div className={"flex gap-4 md:gap-10 w-full"}>
+                    <div className={"relative h-[4.375rem] w-[4.375rem] md:h-[7.313rem] md:w-[7.313rem]"}>
+                        <Image className={"rounded-full"} src={profile.avatar} fill alt={"Images"}/>
+                    </div>
+                    <div className={"flex flex-col justify-between md:py-3"}>
+                        <div className={"text-midBlack dark:text-white md:text-2xl"}>{profile.name}</div>
+                        <a href={profile.profileUrl}
+                           className={"text-blue text-[0.813rem] md:text-base"}>@{profile.username}</a>
+                        <div
+                            className={"text-[0.813rem] text-lightGray dark:text-white md:text-[0.938rem]"}>{profile.createdAt}</div>
+                    </div>
+                </div>
+
+                <div className={"text-[0.813rem] leading-6 mt-8 max-w-[30rem] w-full"}>
+                    {profile.bio || "No bio"}
+                </div>
+
+                <div
+                    className={"bg-midGray dark:bg-background p-4 my-6 md:my-9 font-bold flex justify-between rounded-lg max-w-[30rem] w-full"}>
+                    <div className={"flex-1 flex flex-col items-center gap-2"}>
+                        <div className={"text-xs font-normal text-darkGray dark:text-white"}>Repos</div>
+                        <div className={"text-midBlack dark:text-white"}>{profile.repos}</div>
+                    </div>
+
+                    <div className={"flex-1 flex flex-col items-center gap-2"}>
+                        <div className={"text-xs font-normal text-darkGray dark:text-white"}>Followers</div>
+                        <div className={"text-midBlack dark:text-white"}>{profile.followers}</div>
+                    </div>
+
+                    <div className={"flex-1 flex flex-col items-center gap-2"}>
+                        <div className={"text-xs font-normal text-darkGray dark:text-white"}>Following</div>
+                        <div className={"text-midBlack dark:text-white"}>{profile.following}</div>
+                    </div>
+                </div>
+
+                <div
+                    className={"user-info text-sm leading-5 gap-y-4 grid text-darkGray dark:text-white max-w-[30rem] w-full"}>
+                    <Image src={"/images/icon-location.svg"} width={14} height={20} alt={"location"}/>
+                    <div>{profile.location || "Not Available"}</div>
+
+                    <Image src={"/images/icon-website.svg"} width={20} height={20} alt={"location"}/>
+                    <a className={"hover:underline"} href={profile.blog}>{profile.blog || "Not Available"}</a>
+
+                    <Image src={"/images/icon-twitter.svg"} width={20} height={16} alt={"location"}/>
+                    <div>{profile.twitter || "Not Available"}</div>
+
+                    <div className="relative w-[1.25rem] h-[1.25rem]">
+                        <Image src={"/images/icon-company.svg"} fill alt={"location"}/>
+                    </div>
+                    <div>{profile.company || "Not Available"}</div>
+                </div>
+            </article>
+        </main>
+    );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,26 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
+  darkMode: "class",
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        gray: "#F2F2F2",
+        darkGray: "#4B6A9B",
+        midGray: "#F6F8FF",
+        lightGray: "#697C9A",
+        black: "#222731",
+        midBlack: "#2B3442",
+        blue: "#0079FF",
+        background: "#141D2F",
+        darkBlue: "#1E2A47",
+        lightBlue: "#60ABFF",
+        grayBlue: "#90A4D4",
+        red: "#F74646",
       },
     },
   },
